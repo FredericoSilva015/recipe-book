8f3009a31a5c4e2984464bd6bfb7f4cf1ff5f22a
Fred Silva <fredsilva015@gmail.com>
2020-10-18 17:09:38 +0100

    added styles to the header, added some content to index page

--- src/components/header/component.js
+++ src/components/header/component.js
@@ -1,11 +1,17 @@
 // import { Link } from "gatsby"
-import PropTypes from "prop-types"
-import React from "react"
+import PropTypes from 'prop-types'
+import React from 'react'
+import Search from '../search/component'
+import Style from './style.module.scss'
 
 const Header = ({ siteTitle }) => (
-  <header className="container">
-    <div>
-      <h1>{siteTitle} <span role="img" aria-label="cheecky">🧐</span></h1>
+  <header className={Style.header}>
+    <div className={'container ' + Style.headerWrapper}>
+      <div className={Style.menuWrapper}>
+        <h1 className={Style.title}>{siteTitle} <span role="img" aria-label="cheecky">🧐</span></h1>
+        <button>Menu</button>
+      </div>
+      <Search />
     </div>
   </header>
 )
