1c68bb87b4939798f5dc1315b4169825101f3840
Fred Silva <fredsilva015@gmail.com>
2020-10-18 15:17:53 +0100

    modified header, added new components: search, recipe-list, random-picker

--- src/components/header/component.js
+++ src/components/header/component.js
@@ -1,13 +1,11 @@
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
 const Header = ({ siteTitle }) => (
   <header className="container">
     <div>
-      <h1 style={{ margin: 0 }}>
-        <Link to="/"> {siteTitle} </Link>
-      </h1>
+      <h1>{siteTitle} <span role="img" aria-label="cheecky">🧐</span></h1>
     </div>
   </header>
 )
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,17 +1,13 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react'
+// import { Link } from "gatsby"
 
-import Layout from "../components/layout/component"
-import Image from "../components/image/component"
-import SEO from "../components/seo/component"
+import Layout from '../components/layout/component'
+// import Image from "../components/image/component"
+import SEO from '../components/seo/component'
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <div>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
