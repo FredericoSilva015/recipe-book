855a26ecaa0a01cd5025d7f33fb008054a2c5355
Fred Silva <fredsilva015@gmail.com>
2020-10-29 18:06:54 +0000

    error message to bitwise operators

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,6 +1,8 @@
 export function uuidv4() {
     return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
-        let r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-        return v.toString(16);
+        /* Eslint is set in a way not to allow bit wise operator, but htese are very useful if used correctly*/
+        /* eslint-disable-next-line no-mixed-operators*/
+        let random = Math.random() * 16 | 0, value = c === 'x' ? random : (random & 0x3 | 0x8);
+        return value.toString(16);
     });
 }
